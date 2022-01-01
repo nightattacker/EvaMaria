2e99e3eef66a80b46b5c3f030cfb9b8d49a72ef7
Night Attacker <84122155+nightattacker@users.noreply.github.com>
2022-01-01 21:23:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/EvamariaTG/EvaMaria.git /moviecounter007
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /moviecounter007
 fi
-cd /EvaMaria
+cd /moviecounter007
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
